a6b4e2bc55ca6ec1bad5af9687ac315b795352f8
UonomeUota <okuwing@yahoo.co.jp>
2025-09-21 23:54:18 +0900

    Adobe Fonts読み込みスクリプトを正しいkitIdで更新

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,8 +36,20 @@ export default function RootLayout({
     <html lang="ja">
       <head>
         <Script
-          src="https://use.typekit.net/YOUR_PROJECT_ID.js"
+          id="adobe-fonts"
           strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(d) {
+                var config = {
+                  kitId: 'yzj4wbd',
+                  scriptTimeout: 3000,
+                  async: true
+                },
+                h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\\bwf-loading\\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+              })(document);
+            `
+          }}
         />
       </head>
       <body>{children}</body>
